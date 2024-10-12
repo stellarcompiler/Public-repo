225fe30c3fc7e9b65c3c126884ebf97e9f9a384b
stellarcompiler <aldrinantonyofficial7@gmail.com>
2024-10-12 19:24:57 +0530

    Added file : string pgrm

---
+++ string_test.py
@@ -0,0 +1,14 @@
+ch=""
+str1=""
+ch=input("Enter any string :\n")
+str1=input("Enter any string :\n")
+
+out1=ch.upper()
+out2=ch.lower()
+str2=ch+" "+str1
+char=ch[0:5]
+
+print("Uppercase :",out1)
+print("Lowercase :",out2)
+print("Concatenation Operation :",str2)
+print("Extracted Substring :",char)
